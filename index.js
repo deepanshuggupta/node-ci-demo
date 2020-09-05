6e25fad15f8c5a09d9b2edff595d3dbcb841c430
Deepanshu Gupta <deepanshu.ggupta@gmail.com>
2020-09-05 17:45:30 +0530

    Add log in dockerfiler:

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res, next) => {
-  res.write("Hello Nodejs!");
+  res.write("Hello Nodejs!!");
   res.end();
 });
 
